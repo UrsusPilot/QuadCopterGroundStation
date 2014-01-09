513f99a13bee29ede05b8ccc216b97a277b8c9c6
fboris <fantasyboris@gmail.com>
2014-01-09 14:23:14 +0800

    Add nrf_package struct and package parser

--- main.c
+++ main.c
@@ -4,7 +4,13 @@
 #include "led.h"
 #include "module_nrf24l01.h"
 #include "module_rs232.h"
+typedef struct nrf_package{
 
+	float roll, pitch, yaw;
+	int16_t acc_x, acc_y, acc_z;
+	int16_t gyro_x, gyro_y, gyro_z;
+
+} nrf_package;
 
 void System_Init(void)
 {
@@ -17,12 +23,27 @@ void System_Init(void)
 	 Delay_10ms(2);
 }
 
+void parse_nrf_package( nrf_package* package, uint8_t* buf)
+{
+	memcpy( &(package->roll), &buf[0], sizeof(uint16_t) );
+	memcpy( &(package->pitch), &buf[2], sizeof(uint16_t) );
+	memcpy( &(package->yaw), &buf[4], sizeof(uint16_t) );
+	memcpy( &(package->acc_x), &buf[6], sizeof(uint16_t) );
+	memcpy( &(package->acc_y), &buf[8], sizeof(uint16_t) );
+	memcpy( &(package->acc_z), &buf[10], sizeof(uint16_t) );
+	memcpy( &(package->gyro_x), &buf[12], sizeof(uint16_t) );
+	memcpy( &(package->gyro_y), &buf[14], sizeof(uint16_t) );
+	memcpy( &(package->gyro_z), &buf[16], sizeof(uint16_t) );
+	package->roll = package->roll/100;
+	package->pitch = package->pitch/100;
+	package->yaw = package->yaw/100;
 
+}
 int main(void)
 {
 	uint8_t Sta = ERROR;
 	uint8_t RxBuf[32]= {0};
-
+	nrf_package package;
 	System_Init();
 	printf("Rx mode test...\n\r");
 
